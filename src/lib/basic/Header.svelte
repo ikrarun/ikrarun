3ea83d2724ceaae887b5e13c1b0545b78e3e22ae
wowdeveloper <wowdeveloper@protonmail.com>
2023-07-25 22:37:18 +0530

    navbar fixed

--- src/lib/basic/Header.svelte
+++ src/lib/basic/Header.svelte
@@ -1,6 +1,5 @@
 <script>
 	import { page } from '$app/stores';
-	var link = $page.url.pathname;
 </script>
 
 <header>
@@ -8,20 +7,20 @@
 		class="w-fit p-2 text-white fixed left-0 top-0 sm:top-1/3 flex pointer-events-auto text-xl items-center flex-col mt-1"
 	>
 		<ul class="list-disc flex-col flex space-y-2 text-lg font-normal">
-			<a href={'/'} class={link !== '/' ? 'rounded hover:text-3xl flex ' : 'hidden'}>
+			<a href={'/'} class={$page.url.pathname !== '/' ? 'rounded hover:text-3xl flex ' : 'hidden'}>
 				{`> Home`}
 			</a>
-			<a href={'/about'} class={link !== '/about' ? 'rounded hover:text-3xl flex ' : 'hidden'}>
+			<a href={'/about'} class={$page.url.pathname !== '/about' ? 'rounded hover:text-3xl flex ' : 'hidden'}>
 				{`> About`}
 			</a>
 			<a
 				href={'/projects'}
-				class={link !== '/projects' ? 'rounded hover:text-3xl flex ' : 'hidden'}
+				class={$page.url.pathname !== '/projects' ? 'rounded hover:text-3xl flex ' : 'hidden'}
 			>
 				{`> Projects`}
 			</a>
 
-			<a href={'/skills'} class={link !== '/skills' ? 'rounded hover:text-3xl flex ' : 'hidden'}>
+			<a href={'/skills'} class={$page.url.pathname !== '/skills' ? 'rounded hover:text-3xl flex ' : 'hidden'}>
 				{`> Skills`}
 			</a>
 		</ul>

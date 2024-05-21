ce5ce1150dff06f7cfc5871d6fbaf3b5558399da
ikrarun <iamkrarun@gmail.com>
2024-05-21 10:24:40 +0530

    added partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon({
     iconDir: "src/icons"
-  })],
+  }), partytown()],
   output: "server",
   adapter: vercel()
 });
